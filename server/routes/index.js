434ec8a5d9452ff9934758dfd9abe5239e69f730
Christofer Heimonen <christofer.heimonen@gmail.com>
2016-02-29 23:49:37 +0100

    implemented rest point for registration that does a credential check

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var auth = require('../middleware/auth-middleware');
+var httpStatus = require('http-status');
 
 console.log('setting up routes');
 
@@ -10,12 +11,55 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', function(req, res, next) {
-  console.log('whaat');
   res.render('log-in', {title: 'Paranoia'});
 });
 
-router.get('/create_user', function(req, res, next) {
-  res.render('create-user');
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.post('/register',
+  _validateCredentials,
+  function(req, res, next) {
+  // TODO: insert into db and redirect to successful login page
+  var email = req.body.email;
+  var password = req.body.password;
+  res.status(httpStatus.BAD_REQUEST).send('Ignore me, it went well!');
+});
+
+function _validateCredentials(req, res, next) {
+  // TODO: check if user is already in the database
+  var email = req.body.email;
+  var password = req.body.password;
+  if (email && password) {
+    if (!_validateEmail(email)) {
+      res.status(httpStatus.BAD_REQUEST).send('Please enter a valid email');
+    } else {
+      next();
+    }
+  } else {
+    var noEmail = !email;
+    var noPassword = !password;
+    var status;
+    if (noEmail && noPassword) {
+      status = 'Please enter your credentials';
+    } else if (noEmail) {
+      status = 'Please enter an email'
+    } else {
+      status = 'Please enter a password'
+    }
+    res.status(httpStatus.BAD_REQUEST).send(status);
+  }
+}
+
+function _validateEmail(email)
+{
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
+router.get('/forgot_password', function(req, res, next) {
+  res.render('forgot-password');
 });
 
 router.post('/message', function(req, res, next) {
